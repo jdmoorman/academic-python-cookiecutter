1679bf48a37cc2e55e2f6d5077227a07972db7c7
Jacob Moorman <jdmoorman@math.ucla.edu>
2020-04-11 08:48:42 -0700

    fix github ssh links.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,6 @@ import subprocess
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '.'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
-subprocess.call(['git', 'remote', 'add', 'origin', r'git@github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.github_project_name }}.git'])
+subprocess.call(['git', 'remote', 'add', 'origin', r'git@github.com:{{ cookiecutter.github_username }}/{{ cookiecutter.github_project_name }}.git'])
 subprocess.call(['git', 'remote', '-v'])
 subprocess.call(['git', 'push', '-u', 'origin', 'master'])
